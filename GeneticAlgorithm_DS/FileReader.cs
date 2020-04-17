6ec9d78ce8f4243ebfcec96a3ca79b37ce8e084c
Donovancm <donovan.dcm@gmail.com>
2020-04-17 12:38:17 +0200

    First commit

---
+++ GeneticAlgorithm_DS/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm_DS
+{
+    public class FileReader
+    {
+        public static void ReadFile()
+        {
+
+        }
+    }
+}
