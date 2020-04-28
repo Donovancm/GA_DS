7c3afd3f269c552f2c904053fc6c20bf6138223c
Donovancm <donovan.dcm@gmail.com>
2020-04-28 16:05:31 +0200

    Comments added for future classes

---
+++ GeneticAlgorithm_DS/Calculations/Elitism.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm_DS.Calculations
+{
+    class Elitism
+    {
+        //Methode aanmaken voor nieuwe populatie, hiervan gaan wij de persoon met de hoogste fitness behouden en mee laten doen aan de volgende Iteratie
+        //50 ---> 50 ---> de hoogste fitness bewaren --->
+    }
+}
---
+++ GeneticAlgorithm_DS/Calculations/Mutation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm_DS.Calculations
+{
+    class Mutation
+    {
+        //Methode aanmaken voor Probability Error voor elke cel, elke children
+        //Aangepaste childrens toevoegen aan population
+    }
+}
